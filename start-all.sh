f9a144af220a7cf855ed4833c1d511bac0f6af8a
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-12-05 15:51:31 +0100

    Update start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Chemin vers l'environnement virtuel
-VENV_PATH="/home/michel/brightnessaiv2/brightnessaiv2"
-
-# Activation de l'environnement virtuel
-source "$VENV_PATH/bin/activate"
 
 # Lancement de Gunicorn avec la configuration spécifique
 gunicorn --bind 0.0.0.0:8000 -w 4 streaming_api:app --timeout 1200 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
@@ -13,5 +8,3 @@ gunicorn --bind 0.0.0.0:8001 -w 4 transformers_api:app --timeout 7200 --daemon -
 
 gunicorn --bind 0.0.0.0:8002 -w 4 alter_brain_api:app --timeout 1200 --daemon --access-logfile docs/access.log --error-logfile docs/error.log --capture-output
 
-# Désactivation de l'environnement virtuel (facultatif)
-deactivate
