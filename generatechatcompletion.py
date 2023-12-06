39c2607ca2720f3e74352650245948a3890170a8
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-12-06 15:34:16 +0100

    adaptation de la température

--- generatechatcompletion.py
+++ generatechatcompletion.py
@@ -82,10 +82,14 @@ def generate_chat_completion(consigne, texte, model="gpt-4", model_url=os.enviro
             print("Model URL : " + model_url + "\n" + "HF TOKEN : " + os.environ['HF_API_TOKEN'] + "\n")
             
             client = InferenceClient(model_url, token=os.environ['HF_API_TOKEN'])
-            response = client.text_generation(prompt, max_new_tokens=1024, stream=True)
-            for result in response:
-                print(result, end="", flush="true")
-                yield(result)
+            temperature = 0 # Choisissez une valeur entre 0 et 1
+            response = client.text_generation(
+                prompt,
+                max_new_tokens=1024,
+                stream=True,
+                parameters={"temperature": temperature}
+            )
+
         
         else:
             
@@ -140,10 +144,13 @@ def generate_chat(consigne, texte, system="", model="gpt-4", model_url=os.enviro
             print("Model URL : " + model_url + "\n" + "HF TOKEN : " + os.environ['HF_API_TOKEN'] + "\n")
             
             client = InferenceClient(model_url, token=os.environ['HF_API_TOKEN'])
-            response = client.text_generation(prompt, max_new_tokens=1024, stream=True)
-            for result in response:
-                print(result, end="", flush="true")
-                yield(result)
+            temperature = 0 # Choisissez une valeur entre 0 et 1
+            response = client.text_generation(
+                prompt,
+                max_new_tokens=1024,
+                stream=True,
+                parameters={"temperature": temperature}
+            )
 
         else:   
             #Model = gpt-4-1106-preview 
