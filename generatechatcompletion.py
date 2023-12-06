2335366412b74bc107bf4c4dff54a78de0743367
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-12-06 11:02:10 +0100

    Update generatechatcompletion.py

--- generatechatcompletion.py
+++ generatechatcompletion.py
@@ -141,11 +141,10 @@ def generate_chat(consigne, texte, system, model="gpt-4", model_url=os.environ['
             prompt = str(consigne + "\n Le texte : ###" + texte + " ###\n")  # Construct the prompt from the given consigne and texte
             print("Prompt : " + prompt + "\n")
             print("Model URL : " + model_url + "\n" + "HF TOKEN : " + os.environ['HF_API_TOKEN'] + "\n")
-            yield(lib__hfmodels.stream_hfllm(prompt, os.environ['HF_API_TOKEN'], model_url, 300, 1024))
-            
-            #for content in response:
-            #    print(content)
-            #    yield content            
+            response = lib__hfmodels.stream_hfllm(prompt, os.environ['HF_API_TOKEN'], model_url, 300, 1024)
+            for content in response:
+                    print(content)
+                    yield content          
             
         else:
             if model == "mistral":
