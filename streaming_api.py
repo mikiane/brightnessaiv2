081d5d8c2c7f2b6ccb7fe0277d41be280dc4b9bd
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2024-04-10 21:54:28 +0200

    Update streaming_api.py

--- streaming_api.py
+++ streaming_api.py
@@ -58,7 +58,7 @@ def chat():
     texte = data.get('texte')
     system = data.get('system', '') # Default to '' if no model is provided
     model = data.get('model', 'gpt-4')  # Default to 'gpt-4' if no model is provided
-    temperature = data.get('temperature', 0)  # Default to 0.5 if no temperature is provided
+    temperature = float(data.get('temperature', str(0)))  # Default to 0.5 if no temperature is provided
      
     print("consigne: " + consigne + "\n" + "texte: " + texte + "\n" + "system: " + system + "\n" + "model: " + model + "\n" + "temperature: " + str(temperature) + "\n")
 
