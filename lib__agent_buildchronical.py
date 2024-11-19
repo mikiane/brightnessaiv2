c63d48b98b55da7877881ae4930ad3e5af4da857
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2024-11-19 08:18:37 +0100

    Update Eleven Labs Model

--- lib__agent_buildchronical.py
+++ lib__agent_buildchronical.py
@@ -139,10 +139,10 @@ def texttospeech(text, voice_id, filename):
         "text": text,
         "model_id": "eleven_multilingual_v2",
         "voice_settings": {
-                "stability": 1,
-                "similarity_boost": 1,
-                "style": 0,
-                "use_speaker_boost": False
+                "stability": 0.8,
+                "similarity_boost": 0.8,
+                "style": 0.1,
+                "use_speaker_boost": True
             }
         
         }
