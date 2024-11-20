cb1b01b8733f320976f363808479927da55ab139
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2024-11-20 09:19:27 +0100

    Update lib__agent_buildchronical.py

--- lib__agent_buildchronical.py
+++ lib__agent_buildchronical.py
@@ -139,9 +139,9 @@ def texttospeech(text, voice_id, filename):
         "text": text,
         "model_id": "eleven_multilingual_v2",
         "voice_settings": {
-                "stability": 0.98,
-                "similarity_boost": 0.01,
-                "style": 0.01,
+                "stability": 1,
+                "similarity_boost": 1,
+                "style": 0,
                 "use_speaker_boost": True
             },
         "apply_text_normalization": "auto"
