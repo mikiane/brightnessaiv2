53c032659449a9d11cde3bb12f912fb25c0fa645
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2024-11-24 15:44:37 +0100

    Bugs Imports

--- auto_genweekpodcast_ai.py
+++ auto_genweekpodcast_ai.py
@@ -1,5 +1,6 @@
 
-
+from pydub import AudioSegment
+import os
 import lib__agent_buildchronical
 import random
 import datetime
@@ -9,7 +10,6 @@ import locale
 import lib__transformers
 import json
 import lib__embedded_context
-import os
 import requests
 import time
 import openai
@@ -26,7 +26,7 @@ from googleapiclient.http import MediaFileUpload
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 import requests
-from lib_acast_elevenbas import *
+import lib_acast_elevenbas
 
 # load_dotenv(DOTENVPATH)
 # Load the environment variables from the .env file
@@ -155,7 +155,7 @@ Instructions pour les actualités fournies :
 
 #prompt = "À partir du texte fourni, générer un script de podcast en français d'au moins 30000 signes pour 'L'IA aujourd'hui', présenté par Michel Lévy Provençal, avec l'introduction standard 'Bienvenue dans L'IA aujourd'hui : le podcast de l'IA par l'IA qui vous permet de rester à la page ! Je suis Michel Lévy Provençal, votre hôte' et la conclusion standard 'Voilà qui conclut notre épisode d'aujourd'hui. Merci de nous avoir rejoints et n'oubliez pas de vous abonner pour ne manquer aucune de nos discussions passionnantes. À très bientôt dans L'IA aujourd'hui !'. Adopter un style de revue de presse dynamique avec un ton journalistique engageant caractéristique de Michel Lévy Provençal. Chaque news doit être développée sur au moins 6000 signes, incluant contexte, détails et implications, en expliquant les termes techniques sans simplification excessive. Établir des liens pertinents entre les actualités pour créer une narration fluide. Ignorer les articles trop génériques ou manquant d'informations substantielles. Utiliser des transitions naturelles entre les sujets, des questions rhétoriques pour maintenir l'engagement, et un style narratif incluant le 'nous' inclusif. Le contenu doit être informatif et accessible, équilibrant faits techniques et analyse approfondie, en gardant toujours à l'esprit qu'il s'agit d'une revue de presse destinée à être écoutée."
 #text_final = lib__agent_buildchronical.execute(prompt, '', text_veille, model)
-text_final = call_llm(prompt, text_veille, "", model, 14000)
+text_final = lib_acast_elevenbas.call_llm(prompt, text_veille, "", model, 14000)
 
 print(text_final)
 
