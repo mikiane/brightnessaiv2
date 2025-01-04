9e1fb480d2eb347efd48621b2c69f7593fb70f28
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2025-01-04 12:19:13 +0100

    Update auto_genpodcast_ai.py

--- auto_genpodcast_ai.py
+++ auto_genpodcast_ai.py
@@ -174,6 +174,9 @@ Démarrer directement par le contenu du script généré et donc par "Bonjour à
 # text_final = lib_genpodcasts.call_llm(prompt, text_veille, "", model, 16000)
 text_final = lib_genpodcasts.call_google_llm(prompt, text_veille, "")
 
+prompt_net = "A partir de ce texte, retirer l'éventuelle introduction qui dit 'Here's the plan to generate the script' et renvoyer directement le script qui débute par 'Bonjour à toutes et à tous...' et qui finit par 'À très bientôt dans L'IA aujourd'hui !'."
+text_final = lib_genpodcasts.call_google_llm(prompt_net, text_final, "")
+
 print("\n\n\n ----- RESULTAT DU SCRIPT DE PODCAST ----- \n\n\n")
 print(text_final)
 
